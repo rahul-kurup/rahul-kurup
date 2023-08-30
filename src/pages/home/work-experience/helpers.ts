beda6304a3fae99146e27edf5e5ac91c076b6cb8
rahul kurup <raulsrule@gmail.com>
2023-08-30 12:10:44 +0530

    fix: job desc

--- src/pages/home/work-experience/helpers.ts
+++ src/pages/home/work-experience/helpers.ts
@@ -7,9 +7,11 @@ export default [
     designation: 'Team Lead',
     from: 'Mar 2022',
     techStack: 'React, Node.js, Typescript, Redux etc.',
-    description: `Leading a team of 6 members.
-      I mainly work on Franchise and Workforce projects to help Getir business to
-      grow in existing markets`
+    description: `> Enhanced chegg.com for millions of students by improving performance, functionality, and accessibility during migration.
+    > Took charge of developing and guiding Marketing (SEO) and Legal pages, overseeing the migration of high-traffic pages from older
+    technology to the latest stack.
+    > Developed an automated solution for the product team to easily make their own product and landing pages without having to rely
+    on engineers.`
   },
   {
     company: 'Chegg',
@@ -18,8 +20,11 @@ export default [
     from: 'Jul 2020',
     to: 'Mar 2022',
     techStack: 'React, Nextjs, Typescript, Figma, Contentful, Node.js etc.',
-    description: `Led a team of 8 members, while managing Marketing (SEO) and Legal pages, by
-    converting high-traffic pages from PHP to React/Nextjs stack.`
+    description: `> Enhanced chegg.com for millions of students by improving performance, functionality, and accessibility during migration.
+    > Took charge of developing and guiding Marketing (SEO) and Legal pages, overseeing the migration of high-traffic pages from older
+    technology to the latest stack.
+    > Developed an automated solution for the product team to easily make their own product and landing pages without having to rely
+    on engineers.`
   },
   {
     company: 'Nagarro',
@@ -28,45 +33,22 @@ export default [
     from: 'Dec 2018',
     to: 'Jul 2020',
     techStack: 'Angular (with SSR), Typescript, Node.js etc',
-    description: `Led a team of 6 members.
-    I got promoted to Associate Lead from Senior Software Engineer within my work tenure
-    based on the team management, work ethics and progress I maintained in the client
-    project.
-    Worked on Revenue Management application for a high-profile client which would cater
-    daily business needs like order management, recurring payments, report generation etc`
-  },
-  {
-    company: 'R Systems International',
-    website: 'https://www.rsystems.com/',
-    designation: 'Senior Software Developer',
-    from: 'Apr 2018',
-    to: 'Dec 2018',
-    techStack: 'Angular (with SSR), Typescript, C#, ASP.NET Core 2.1 etc',
-    description: `Managing an e-card commerce website adding new feature support as per client
-    requirement.`
+    description: `> Led the development and management of a Revenue Management application tailored for a high-profile client.
+    > Oversaw the implementation of features satisfying crucial daily business requirements, including order management, recurring
+    payments, and report generation.
+    > Delivered a comprehensive solution that effectively streamlined business operations and optimized revenue management processes`
   },
   {
     company: 'Planetcast Media Services',
     website: 'https://www.planetc.net/',
     designation: 'Software Developer',
-    from: 'May 2017',
-    to: 'Apr 2018',
+    from: 'September 2016',
+    to: 'December 2018',
     techStack:
       'Angular 4.x, Typescript, WinForms, WPF, C#, ASP.NET Core 2.1 etc',
-    description: `Worked on multiple inhouse software tending towards satellite/dish streaming,
-    monitoring, live charts/logs, and timeline management. Managing proper syncing of
-    audio/video/subtitle/logo for each show that would be streamed by the partner channel;
-    along with reading media hardware meta information using MIB.`
-  },
-  {
-    company: 'Richa Industries',
-    website: 'https://www.richa.in/',
-    dissolved: true,
-    designation: 'Software Developer',
-    from: 'Sep 2016',
-    to: 'Apr 2017',
-    techStack: 'WinForms, VB.Net, Crystal Report, MSSQL etc',
-    description: `Supporting existing inhouse PEB/Texting software, adding new features and reports`
+    description: `Noida Area, India
+    > Created multiple in-house software applications for satellite/dish streaming, monitoring, live charts/logs, and timeline management.
+    > Ensured seamless synchronization of audio/video/subtitle/logo for partner channel shows`
   },
   {
     company: 'Cisoft Technologies',
@@ -75,9 +57,8 @@ export default [
     from: 'May 2014',
     to: 'Aug 2016',
     techStack: 'WinForms, ASP.Net, VB.Net, Crystal Report, MSSQL etc',
-    description: `Supporting and testing inhouse ERP software that be supplied to multiple schools and
-    colleges. Generating live reports and dynamic timetable; including fees/library/payroll
-    management.`
+    description: `> Contributed to testing and development of in-house ERP software for schools and colleges.
+    > Automated live reports, dynamic timetables, and management of fees, library, and payroll`
   }
 ].map((m, id) => ({ ...m, id: id + m.company + m.from })) as (TimelineProps & {
   id: string;

5918478e00660649fd1d06462d67a61dd65bb809
rahul kurup <rahul.kurup@hotmail.com>
2026-03-12 01:34:26 +0530

    fix: vuln and proper error msg for missing api key

--- src/pages/api/visitor.p.ts
+++ src/pages/api/visitor.p.ts
@@ -1,30 +1,33 @@
 import config from '@config';
-import { Resend } from 'resend';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Resend } from 'resend';
 
 const { mail } = config.serverOnly;
-const resend = new Resend(mail.resend.apiKey);
 
 export default async function handler(
   { query, headers }: NextApiRequest,
   res: NextApiResponse
 ) {
-  const name = (query.name || '') as string;
-  if (name?.trim()) {
-    try {
-      const mailerResponse = await resend.emails.send({
-        to: mail.to,
-        from: mail.from,
-        subject: `You have a new visitor - ${name}`,
-        text: `"${name}" visited the website on ${new Date().toISOString()}. Originated from ${
-          headers.host
-        }`
-      });
-      console.log('Email sent', mailerResponse);
-      res.status(200).json(true);
-    } catch (error) {
-      console.error('Email send error', error);
-      res.status(200).json(false);
+  if (mail.resend.apiKey) {
+    const name = (query.name || '') as string;
+    if (name?.trim()) {
+      try {
+        const resend = new Resend(mail.resend.apiKey);
+        const mailerResponse = await resend.emails.send({
+          to: mail.to,
+          from: mail.from,
+          subject: `You have a new visitor - ${name}`,
+          text: `"${name}" visited the website on ${new Date().toISOString()}. Originated from ${
+            headers.host
+          }`
+        });
+        console.log('Email sent', mailerResponse);
+        res.status(200).json({ success: true });
+      } catch (error) {
+        res.status(503).json({ success: 'Email send error', error });
+      }
     }
+  } else {
+    res.status(503).json({ success: false, reason: 'missing apikey' });
   }
 }
